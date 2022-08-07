0b8828d1a320877196d8d8059b7b1c48e0ab4860
Joshua Famiroju <joshuafamiroju@hotmail.com>
2022-08-07 16:11:33 +0100

    committed

---
+++ string-methods.js
@@ -0,0 +1,28 @@
+//slice() method
+const names = ['Joshua', 'Famiroju', 'Prosper', 'Olaoluwa']
+
+console.log(names.slice(1))
+console.log(names.slice(2))
+console.log(names.slice(3))
+console.log(names.slice(-2))
+console.log(names.slice(-1))
+
+//toUpperCase() Method
+
+const sentence = 'I am a Web developer'
+const changeCase = sentence.toUpperCase()
+console.log(changeCase)
+
+//toLowerCase() Method
+
+const sentence2 = 'I am a Web developer'
+const changeCase2 = sentence.toLowerCase()
+console.log(changeCase2)
+
+//trim() Method
+
+const fullName = '    Joshua Famiroju'
+console.log(fullName)
+
+const trimName = fullName.trim()
+console.log (trimName)
