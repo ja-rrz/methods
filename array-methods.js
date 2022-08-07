04f48a82797000f596da10a48241f063e543f821
Joshua Famiroju <joshuafamiroju@hotmail.com>
2022-08-07 13:55:43 +0100

    committed

--- array-methods.js
+++ array-methods.js
@@ -1,4 +1,26 @@
-Array.isArray([1, 2, 3]);  // true
-Array.isArray({foo: 123}); // false
-Array.isArray('foobar');   // false
-Array.isArray(undefined);  // false
+// shift() Method
+const myArray = [1, 2, 3];
+
+const firstValue = myArray.shift();
+
+console.log(myArray);
+
+console.log(firstValue);
+
+//some() Method
+const array = [1, 2, 3, 4];
+
+const odd = (element) => element % 1 === 0;
+
+console.log(array.some(odd));
+
+//reverse() method
+const array2 = ['One', 'Two', 'Three']
+console.log('Normal:', array2)
+
+const reversed = array2.reverse();
+console.log('reversed:', reversed);
+
+
+
+
